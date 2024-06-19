ce82b9ae82626821952eba08fd12486ae9d07fba
1-arjayabalan-0 <arjayabalan09@gmail.com>
2024-06-19 20:08:19 +0530

    microservice updated-1

--- LoginService/src/main/java/com/leiten/loginservice/LoginServiceApplication.java
+++ LoginService/src/main/java/com/leiten/loginservice/LoginServiceApplication.java
@@ -3,7 +3,9 @@ package com.leiten.loginservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@EnableEurekaClient
 public class LoginServiceApplication {
 	public static void main(String args[]) {
 		SpringApplication.run(LoginServiceApplication.class, args);
